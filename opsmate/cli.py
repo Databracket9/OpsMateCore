737033d61cf3d4dec479401ce3204a5af2c2f7ab
jayachandra27 <jayachandra.s.reddy@outlook.com>
2023-09-14 18:19:06 +0530

    configured and build inital installable cli

--- opsmate/cli.py
+++ opsmate/cli.py
@@ -1,18 +1,17 @@
 import typer
-from utils.project_setup import base_setup
+from opsmate.utils.project_setup import base_setup
 
 app = typer.Typer()
-# setup = base_setup()
+setup = base_setup()
 
 @app.command()
-def primesetup(projname: str):
-    print(projname)
-    base_setup().project_def(project_name=projname)
+def projsetup(projname: str):
+    setup.project_def(project_name=projname)
 
 @app.command()
 def hello(name: str):
     print(f"Hello {name}")
 
 
-if __name__ == "__main__":
+def entrypoint():
     app()
--- opsmate/utils/project_setup.py
+++ opsmate/utils/project_setup.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 class base_setup:
 
@@ -11,11 +12,17 @@ class base_setup:
         if not os.path.exists(project_name):
             os.mkdir(project_name)
 
-        file_path = os.path.join(base_directory, project_name, 'test_file.json')
+        file_path = os.path.join(base_directory, project_name, 'config.yaml')
+        data = {
+                "name": "Jay", 
+                "projectName": project_name,
+                "email":"databracket9@gmail.com"
+                }
         with open(file=file_path, mode="w") as file:
-            file.write(
-                '{"name": "Jay", "email":"databracket9@gmail.com"}'
-            )
+            yaml.dump(data, file, default_flow_style=False)
+            # file.write(
+            #     '{"name": "Jay", "email":"databracket9@gmail.com"}'
+            # )
         
 
 
